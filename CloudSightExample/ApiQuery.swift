96f380fc48f940716ffcacfad437365dce3f56d2
Xu Xinyi <xuxinyi@wireless-10-147-131-121.public.utexas.edu>
2018-10-19 15:58:30 -0500

    Started multiple API query calls

--- CloudSightExample/ApiQuery.swift
+++ CloudSightExample/ApiQuery.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+class ApiQuery {
+    var image : UIImage!
+    var endpoint : String!
+    var headers : [(name: String, value: String)]!
+    var key : String!
+    
+    
+}
+
+class CloudSightApiQuery : ApiQuery {
+    init(image: UIImage){
+        super.init()
+        self.image = image
+        self.endpoint = "https://api.cloudsight.ai/v1/images"
+        self.headers = [(name: "authorization", value: "CloudSight C38_disOS8RllVk3OQ5cDw"),
+                        (name: "cache-control", value: "no-cache"),
+                        (name: "content-type" , value: "application/json")]
+        
+    }
+}
