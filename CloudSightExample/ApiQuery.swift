7cb1aaf9895af743d50e230e46a5605a31b82676
Xu Xinyi <xuxinyi@wireless-10-147-131-121.public.utexas.edu>
2018-10-19 14:51:41 -0500

    First commit

---
+++ CloudSightExample/ApiQuery.swift
@@ -0,0 +1,9 @@
+//
+//  ApiQuery.swift
+//  CloudSightExample
+//
+//  Created by Xu Xinyi on 2018/10/19.
+//  Copyright © 2018 CloudSight, Inc. All rights reserved.
+//
+
+import Foundation
---
+++ CloudSightExample/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  CloudSightExample
+//
+
+import UIKit
+import CloudSight
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CloudSightQueryDelegate {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    var cloudsightQuery: CloudSightQuery!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        CloudSightConnection.sharedInstance().consumerKey = "C38_disOS8RllVk3OQ5cDw";
+        CloudSightConnection.sharedInstance().consumerSecret = "HAwGhA4TNmqOo0morWWYpA";
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+
+        // Check to see if the Camera is available
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+
+            // Show the UIImagePickerController view
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            print("Cannot access the camera");
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+        // Dismiss the UIImagePickerController
+        self.dismiss(animated: true, completion: nil)
+        
+        // Assign the image reference to the image view to display
+        let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as! UIImage
+        imageView.image = image
+        
+        // Create JPG image data from UIImage
+        let imageData = image.jpegData(compressionQuality: 0.8)
+        
+        cloudsightQuery = CloudSightQuery(image: imageData,
+                                          atLocation: CGPoint.zero,
+                                          withDelegate: self,
+                                          atPlacemark: nil,
+                                          withDeviceId: "device-id")
+        cloudsightQuery.start()
+        activityIndicatorView.startAnimating()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func cloudSightQueryDidFinishUploading(_ query: CloudSightQuery!) {
+        print("cloudSightQueryDidFinishUploading")
+    }
+    
+    func cloudSightQueryDidFinishIdentifying(_ query: CloudSightQuery!) {
+        print("cloudSightQueryDidFinishIdentifying")
+
+        // CloudSight runs in a background thread, and since we're only
+        // allowed to update UI in the main thread, let's make sure it does.
+        DispatchQueue.main.async {
+            self.resultLabel.text = query.name()
+            self.activityIndicatorView.stopAnimating()
+        }
+    }
+    
+    func cloudSightQueryDidFail(_ query: CloudSightQuery!, withError error: Error!) {
+        print("CloudSight Failure: \(error)")
+    }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
